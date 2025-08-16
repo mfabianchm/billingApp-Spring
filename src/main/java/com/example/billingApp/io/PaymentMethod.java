4502d0f26dc71ce55663be0ebed4f583d283625f
mfabianchm <mfchongm@hotmail.com>
2025-08-16 12:45:22 -0600

    "Dashboard controller created, Order and Items Entities implemented and Services for Payment implemented"

---
+++ src/main/java/com/example/billingApp/io/PaymentMethod.java
@@ -0,0 +1,5 @@
+package com.example.billingApp.io;
+
+public enum PaymentMethod {
+    CASH, UPI
+}
---
+++ src/main/java/com/example/billingApp/service/OrderService.java
@@ -0,0 +1,26 @@
+package com.example.billingApp.service;
+
+import com.example.billingApp.io.Order.OrderRequest;
+import com.example.billingApp.io.Order.OrderResponse;
+import com.example.billingApp.io.PaymentVerificationRequest;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public interface OrderService {
+
+    OrderResponse createOrder(OrderRequest orderRequest);
+
+    void deleteOrder(String orderId);
+
+    List<OrderResponse> getLatestOrders();
+
+    OrderResponse verifyPayment(PaymentVerificationRequest request);
+
+    Double sumSalesByDate(LocalDate date);
+
+    Long countByOrderDate(LocalDate date);
+
+    List<OrderResponse> findRecentOrders();
+
+}
